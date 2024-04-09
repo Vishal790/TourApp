3870bf6c0679a63f57af24ff3eba24e9b06c2614
Vishal Srivastava <vishal.vs690@gmail.com>
2024-04-09 10:39:41 +0530

    first commit

---
+++ src/components/Tours.jsx
@@ -0,0 +1,25 @@
+import Card from './Card'
+
+
+
+export default function Tours({ tours,removeTour }) {
+
+    return (
+        <div className='container'>
+            <div>
+                <h2 className='title'>Tour App</h2>
+            </div>
+            <div className='cards'>
+                {
+                    tours.map((tour) => {
+                        return <Card key={tour.id} {...tour}removeTour={removeTour} />
+                    })
+
+                }
+
+            </div>
+        </div>
+
+
+    )
+}
